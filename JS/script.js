37d084f6dddc4002cdee6aff062a3202c5d94785
Michał Wójcik <michalwojcik@macbook-pro-micha.home>
2020-11-07 14:31:15 +0100

    My website - first steps

---
+++ JS/script.js
@@ -0,0 +1,10 @@
+﻿let prevScrollpos = window.pageYOffset;
+window.onscroll = () => {
+  let currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
